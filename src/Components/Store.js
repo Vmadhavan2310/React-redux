7c907ce2e4de8daf193304a84e5fc7c940205fcd
vinothM <vinodmad@34gmail.com>
2023-11-27 16:42:23 +0530

    Initiated Store for redux

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import CakeReducer from './Cake/CakeReducer';
 import IceCreamReducer from './IceCream.js/IceCreamReducer';
+import UserAPI from './FetchWithRedux/FetchReducer';
 export const Store=configureStore({
     reducer:{
         Cake:CakeReducer,
-        Ice:IceCreamReducer
+        Ice:IceCreamReducer,
+        fetchData:UserAPI
     }
 })
